ae4e75a78eb7984a1a60e6525901c7678d22c296
RD99 <47771507+RD99@users.noreply.github.com>
2019-11-01 22:25:24 +0530

    img changed jimp used

--- index.js
+++ index.js
@@ -10,58 +10,74 @@ const handler = tfn.io.fileSystem("model/model.json");
 
 var getPixels = require("get-pixels");
 var path = require("path");
+var Jimp = require("jimp");
 const port = process.env.PORT || 5000;
 app.use(fileupload());
 app.post("/predict", (req, res) => {
   const file = req.files.file;
 
-  const a = new Uint8Array(file.data);
-  //console.log(tf.tensor(a));
+  Jimp.read(file.data, (err, image) => {
+    if (err) throw err;
 
-  //const model = tf.loadLayersModel("Model_with_15_Epochs.h5");
-  const stream = fs.createWriteStream("image.jpg");
-  stream.write(file.data);
-  stream.close();
-  //console.log("model loader");
+    image.resize(300, 300).write("image2.jpg");
+    let img2 = new Array(1);
 
-  //const buffer = tf.buffer([1, 400, 400, 3], "int32", a);
-  //console.log(buffer);
-  stream.on("finish", function() {
-    getPixels("image.jpg", function(err, pixels) {
-      if (err) {
-        console.log("Bad image path");
-        return;
+    img2[0] = new Array(300);
+    for (let x = 0; x < 300; x++) {
+      img2[0][x] = new Array(300);
+      for (let y = 0; y < 300; y++) {
+        let pixelcolor = image.getPixelColor(x, y);
+        let rgbavalue = Jimp.intToRGBA(pixelcolor);
+        img2[0][x][y] = new Array(3);
+        img2[0][x][y][0] = rgbavalue["r"];
+        img2[0][x][y][1] = rgbavalue["g"];
+        img2[0][x][y][2] = rgbavalue["b"];
       }
-      //console.log("got pixels", pixels);
-      const unit = new Uint8Array(pixels.data);
-      //console.log(pixels.data.length);
-      let img = new Array(1);
-      img[0] = new Array(300);
-      for (let x = 0; x < 300; x++) {
-        img[0][x] = new Array(300);
-        for (let y = 0; y < 300; y++) {
-          img[0][x][y] = new Array(3);
-          img[0][x][y][0] = unit[x * 300 + y * 4 + 0];
-          img[0][x][y][1] = unit[x * 300 + y * 4 + 1];
-          img[0][x][y][2] = unit[x * 300 + y * 4 + 2];
-        }
-      }
-      const unittensor = tf.tensor(unit);
-      //const buffer = tf.buffer([1, 300, 300, 3], "int32", img);
-      //console.log(tf.tensor(img));
-      tf.loadLayersModel(handler).then(model => {
-        //const bowData = bow(file, true);
-        //var data = tf.tensor2d(bowData, [1, bowData.length]);
-        const prediction = model.predict(tf.tensor(img));
+    }
 
-        //console.log(model);s
-        //console.log("prediction" + prediction);
-        //console.log(prediction.dataSync());
-
-        res.send(prediction.dataSync());
-      });
+    tf.loadLayersModel(handler).then(model => {
+      const prediction = model.predict(tf.tensor(img2));
+      // console.log("prediction" + prediction);
+      // console.log(prediction.dataSync());
+      res.send(prediction.dataSync());
     });
   });
+  // const stream = fs.createWriteStream("image.jpg");
+  // stream.write(file.data);
+  // stream.close();
+
+  // stream.on("finish", function() {
+  //   getPixels("image.jpg", function(err, pixels) {
+  //     if (err) {
+  //       console.log("Bad image path");
+  //       return;
+  //     }
+  //     //console.log("got pixels", pixels.data);
+
+  //     const unit = new Uint8Array(pixels.data);
+
+  //     //console.log(pixels.data.length);
+  //     let img = new Array(1);
+  //     img[0] = new Array(300);
+  //     for (let x = 0; x < 300; x++) {
+  //       img[0][x] = new Array(300);
+  //       for (let y = 0; y < 300; y++) {
+  //         img[0][x][y] = new Array(3);
+  //         img[0][x][y][0] = unit[x * 300 + y * 4 + 0];
+  //         img[0][x][y][1] = unit[x * 300 + y * 4 + 1];
+  //         img[0][x][y][2] = unit[x * 300 + y * 4 + 2];
+  //       }
+  //     }
+
+  //       tf.loadLayersModel(handler).then(model => {
+  //         const prediction = model.predict(tf.tensor(img));
+  //         // console.log("prediction" + prediction);
+  //         // console.log(prediction.dataSync());
+  //         res.send(prediction.dataSync());
+  //       });
+
+  //   });
+  // });
 
   // console.log(
   //   file.data
